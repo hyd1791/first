5270bc043e94934987453b9b493848e86b3cfce5
hyd1791 <hyd1791@gmail.com>
2025-03-05 15:41:51 +0900

    안녕하세요

--- first/views.py
+++ first/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {'name1':'함영도',
-               'name2' :'곱창',
-               'name3' : '아무개',}
+    context = {'name':'함영도',
+               'hobby' :'곱창',
+               'interest' : '아무개',}
     return render(request,'first/home.html', context)
 # Create your views here.
